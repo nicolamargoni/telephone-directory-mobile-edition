f9fa3d8bbd13357a4c375d754fe6df6fd2d1c2e4
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-23 15:42:29 +0100

    create first acceptance test: valid entry insertion

--- ios/Telephone Directory/Telephone DirectoryUITests/Telephone_DirectoryUITests.swift
+++ ios/Telephone Directory/Telephone DirectoryUITests/Telephone_DirectoryUITests.swift
@@ -1,36 +1,27 @@
-//
-//  Telephone_DirectoryUITests.swift
-//  Telephone DirectoryUITests
-//
-//  Created by Nicola Margoni on 21/11/17.
-//  Copyright © 2017 Nicola Margoni. All rights reserved.
-//
-
 import XCTest
 
 class Telephone_DirectoryUITests: XCTestCase {
-        
+    var app: XCUIApplication!
+    
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testValidEntryInsertion() {
+        app.launch()
+        app.navigationBars.buttons["Add"].tap()
+        app.tables.cells.textFields["firstName"].typeText("Mario")
+        app.tables.cells.textFields["lastName"].tap()
+        app.tables.cells.textFields["lastName"].typeText("Rossi")
+        app.tables.cells.textFields["telephoneNumber"].tap()
+        app.tables.cells.textFields["telephoneNumber"].typeText("+39 340 1234567")
+        app.navigationBars.buttons["Done"].tap()
+        let cell = app.tables.cells.containing(.staticText, identifier: "Mario Rossi").element
+        XCTAssertTrue(cell.exists)
     }
-    
 }
