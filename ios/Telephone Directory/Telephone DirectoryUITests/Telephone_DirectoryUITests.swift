8a87990363fa63c625e4b7d6ae06c7d10ed7c496
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-24 15:03:17 +0100

    add acceptance test : when user type a empty first name will show a error

--- ios/Telephone Directory/Telephone DirectoryUITests/Telephone_DirectoryUITests.swift
+++ ios/Telephone Directory/Telephone DirectoryUITests/Telephone_DirectoryUITests.swift
@@ -24,4 +24,17 @@ class Telephone_DirectoryUITests: XCTestCase {
         let cell = app.tables.cells.containing(.staticText, identifier: "Mario Rossi").element
         XCTAssertTrue(cell.exists)
     }
+    
+    func testEntryWithNotValidFirstNameWillShowAnError(){
+        app.launch()
+        app.navigationBars.buttons["Add"].tap()
+        app.tables.cells.textFields["firstName"].typeText("")
+        app.tables.cells.textFields["lastName"].tap()
+        app.tables.cells.textFields["lastName"].typeText("Rossi")
+        app.tables.cells.textFields["telephoneNumber"].tap()
+        app.tables.cells.textFields["telephoneNumber"].typeText("+39 340 1234567")
+        app.navigationBars.buttons["Done"].tap()
+        let error = app.tables.cells.staticTexts["first name can't be empty"]
+        XCTAssertTrue(error.exists)
+    }
 }
